892cda95d63d80ab3d396e9c496534155275c81a
Elias Cotrim <cotrim.elias@gmail.com>
2021-07-29 20:43:57 -0300

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,50 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Elias Cotrim Portfolio",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "",
-        spaceId: "",
-      },
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Elias Cotrim Portfolio",
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+    plugins: [{
+            resolve: "gatsby-source-contentful",
+            options: {
+                accessToken: "3g84W_-K9kXAEobOtqecZnKnwkKZm6ORR6VhXCI8kz4",
+                spaceId: "ezg6vznwq1va",
+            },
+        },
+        "gatsby-plugin-sass",
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-image",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "G-RX3DYPSM90",
+            },
+        },
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+    ],
+}
